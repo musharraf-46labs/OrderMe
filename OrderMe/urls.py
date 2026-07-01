4208437ba25a0fab75657511cbd385b479a55cc9
musharraf-46labs <51646736+musharraf-46labs@users.noreply.github.com>
2026-07-01 19:08:16 +0500

    Add test URL path to urlpatterns

--- OrderMe/urls.py
+++ OrderMe/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test.site.urls),
     path('restraunt/', include('restraunts.urls')),
     path('res_accounts/', include('res_accounts.urls')),
     path('', views.homepage),
